86f5cc3f426a76df0b1fb5b4a2d19bf5f1eca8c6
StevePro7 <steveprozune@hotmail.com>
2017-03-08 11:31:07 +0000

    Add title

--- dev/engine/locale_manager.h
+++ dev/engine/locale_manager.h
@@ -2,5 +2,7 @@
 #define _LOCALE_MANAGER_H_
 
 #define LOCALE_BLANK	"                              "
+#define LOCALE_TITLE1	"CANDYKID"
+#define LOCALE_TITLE2	"  DEMO  "
 
 #endif//_LOCALE_MANAGER_H_
--- dev/main.c
+++ dev/main.c
@@ -18,9 +18,11 @@ void main (void)
 	//engine_font_manager_draw_data(hacker_music, 10, 10);
 	//engine_tree_manager_draw_treeXY(4, 4);
 	
-	//engine_font_manager_draw_text("SGB ALB", 0, 0);
 	engine_tree_manager_draw_border();
 	engine_tree_manager_draw_inside();
+
+	engine_font_manager_draw_text(LOCALE_TITLE1, 8, 11);
+	engine_font_manager_draw_text(LOCALE_TITLE2, 8, 12);
 	SMS_displayOn();
 	for (;;)
 	{
