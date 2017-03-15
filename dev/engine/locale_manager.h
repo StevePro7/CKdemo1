67e92729421b0990e3740f9f595af03f0d70ab98
SteveProXNA <steven_boland@hotmail.com>
2017-03-15 10:23:36 +0000

    Update move text

--- dev/engine/locale_manager.h
+++ dev/engine/locale_manager.h
@@ -11,16 +11,15 @@
 #define LOCALE_SIDE2		"  KID  "
 
 #define LOCALE_MOVE0		"JOY #1"
-#define LOCALE_MOVE00		"------"
-#define LOCALE_MOVE1		"KID UP"
-#define LOCALE_MOVE2		"ADI DOWN"
-#define LOCALE_MOVE3		"SUZ LEFT"
-#define LOCALE_MOVE4		"PRO RGHT"
-
-#define LOCALE_MOVE5		"PRESS #1"
-#define LOCALE_MOVE6		"START IT"
-#define LOCALE_MOVE7		"PRESS #2"
-#define LOCALE_MOVE8		"SOUND FX"
+#define LOCALE_MOVE1		"------"
+#define LOCALE_MOVE2		"KID UP"
+#define LOCALE_MOVE3		"ADI DOWN"
+#define LOCALE_MOVE4		"SUZ LEFT"
+#define LOCALE_MOVE5		"PRO RGHT"
+#define LOCALE_MOVE6		"PRESS #1"
+#define LOCALE_MOVE7		"START IT"
+#define LOCALE_MOVE8		"PRESS #2"
+#define LOCALE_MOVE9		"SOUND FX"
 
 
 #define LOCALE_STEVEPRO1	"STEVEPRO"
--- dev/engine/text_manager.h
+++ dev/engine/text_manager.h
@@ -11,20 +11,20 @@ void engine_text_manager_draw()
 	engine_font_manager_draw_text(LOCALE_SIDE1, 24, 0);
 	engine_font_manager_draw_text(LOCALE_SIDE2, 24, 1);
 
-	move = 5;
-	engine_font_manager_draw_text(LOCALE_MOVE0, 24, move -1);
-	engine_font_manager_draw_text(LOCALE_MOVE00, 24, move + 0);
+	move = 4;
+	engine_font_manager_draw_text(LOCALE_MOVE0, 24, move + 0);
 	engine_font_manager_draw_text(LOCALE_MOVE1, 24, move + 1);
 	engine_font_manager_draw_text(LOCALE_MOVE2, 24, move + 2);
 	engine_font_manager_draw_text(LOCALE_MOVE3, 24, move + 3);
 	engine_font_manager_draw_text(LOCALE_MOVE4, 24, move + 4);
+	engine_font_manager_draw_text(LOCALE_MOVE5, 24, move + 5);
 
 	move += 4;
-	engine_font_manager_draw_text(LOCALE_MOVE5, 24, move + 5);
 	engine_font_manager_draw_text(LOCALE_MOVE6, 24, move + 6);
-	move++;
 	engine_font_manager_draw_text(LOCALE_MOVE7, 24, move + 7);
+	move++;
 	engine_font_manager_draw_text(LOCALE_MOVE8, 24, move + 8);
+	engine_font_manager_draw_text(LOCALE_MOVE9, 24, move + 9);
 
 	engine_font_manager_draw_text(LOCALE_STEVEPRO1, 24, 22);
 	engine_font_manager_draw_text(LOCALE_STEVEPRO2, 24, 23);
