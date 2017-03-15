3a06f760576847440b9156eba3a7c45db575b32b
SteveProXNA <steven_boland@hotmail.com>
2017-03-15 10:21:21 +0000

    Update text manager

--- dev/engine/locale_manager.h
+++ dev/engine/locale_manager.h
@@ -10,6 +10,19 @@
 #define LOCALE_SIDE1		"=CANDY="
 #define LOCALE_SIDE2		"  KID  "
 
+#define LOCALE_MOVE0		"JOY #1"
+#define LOCALE_MOVE00		"------"
+#define LOCALE_MOVE1		"KID UP"
+#define LOCALE_MOVE2		"ADI DOWN"
+#define LOCALE_MOVE3		"SUZ LEFT"
+#define LOCALE_MOVE4		"PRO RGHT"
+
+#define LOCALE_MOVE5		"PRESS #1"
+#define LOCALE_MOVE6		"START IT"
+#define LOCALE_MOVE7		"PRESS #2"
+#define LOCALE_MOVE8		"SOUND FX"
+
+
 #define LOCALE_STEVEPRO1	"STEVEPRO"
 #define LOCALE_STEVEPRO2	"(C) 1984"
 
